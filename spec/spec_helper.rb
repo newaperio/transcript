face624ce7f6dbef1a5b7bc551db5610894605e9
Logan Leger <logan@loganleger.com>
2016-08-23 11:11:25 -0500

    Setup SimpleCov to report code coverage to CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,30 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV.fetch("COVERAGE", false)
+  require "simplecov"
+
+  if ENV["CIRCLE_ARTIFACTS"]
+    dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
+    SimpleCov.coverage_dir(dir)
+  end
+
+  if ENV['CODECLIMATE_REPO_TOKEN']
+    require 'codeclimate-test-reporter'
+    CodeClimate::TestReporter.start
+
+    SimpleCov.formatters = [
+      SimpleCov::Formatter::HTMLFormatter,
+      CodeClimate::TestReporter::Formatter
+    ]
+  end
+
+  SimpleCov.configure do
+    load_profile "test_frameworks"
+  end
+
+  SimpleCov.start
+end
+
 require 'transcript'
 
 ENV["RAILS_ENV"] = "test"
@@ -7,7 +33,6 @@ require "dummy/config/environment"
 
 require "shoulda-matchers"
 require "factory_girl_rails"
-
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
--- transcript.gemspec
+++ transcript.gemspec
@@ -4,10 +4,12 @@ require 'transcript/version'
 Gem::Specification.new do |spec|
   spec.add_development_dependency "ammeter", "~> 1.1"
   spec.add_development_dependency "bundler", "~> 1.12"
+  spec.add_development_dependency "codeclimate-test-reporter", "~> 0.6"
   spec.add_development_dependency "factory_girl_rails", "~> 4.2"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "shoulda-matchers", "~> 3.1"
+  spec.add_development_dependency "simplecov", "~> 0.12"
   spec.add_development_dependency "sqlite3", "~> 1.3"
   spec.add_runtime_dependency "rails", "~> 5.0"
   spec.authors = ["Logan Leger", "NewAperio"]
